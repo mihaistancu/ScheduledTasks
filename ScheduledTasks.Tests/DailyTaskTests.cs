39e8f50323486dbc37dfc7f78110d74bd879d2f4
mihai <stancu_meehigh@yahoo.com>
2016-08-23 23:32:29 +0300

    Added first unit test: can create a daily task

---
+++ ScheduledTasks.Tests/DailyTaskTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScheduledTasks.Tests
+{
+    [TestClass]
+    public class DailyTaskTests
+    {
+        [TestMethod]
+        public void DailyTaskCanBeCreated()
+        {
+            DailyTask task = new DailyTask();
+        }
+    }
+}
